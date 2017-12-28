4b14b16ea1653fe9df5ab799b24e631ce72bafee
霧卦 <1103108147@gm.kuas.edu.tw>
2017-12-28 08:41:35 +0800

    殘骸
    如題 姑且可以算雛形

---
+++ src/circlePath.py
@@ -0,0 +1,19 @@
+class CirclePath():
+    
+    def __init__(self,
+            nowx,
+            nowy,
+            self.finalX,
+            self.finalY,
+            radius,
+            self.clockwise = 0#0為順時針,1為逆時針,若未給值預設為0
+        ):
+        
+        self.enabled = True
+        self.omega = None
+        self.s = 
+    
+    def NextCoor(nowx, nowy):
+        if self.enabled = True:
+            
+            
