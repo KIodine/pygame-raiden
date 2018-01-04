ffc6ad7374e89037515dde3c8cd1d34172729361
KIodine <33240381+KIodine@users.noreply.github.com>
2018-01-04 10:59:16 +0800

    Add todo list.

---
+++ todos.py
@@ -0,0 +1,68 @@
+'''TODO
+    Pyden todo list.
+
+    Priority: 0 > 1 > 2 ...
+
+    1. Menu #0
+        Form:
+            Start
+            Rank
+            Quit
+        psuedo code:
+            def menu():
+                while Menu:
+                    (some operation...)
+                    if condition:
+                        main()
+                    elif condition2:
+                        rank()
+                (terminate...)
+            return
+
+            if __name__ == '__main__':
+                menu()
+
+            pygame.quit()
+            sys.exit(0)
+
+
+    2. Skill module #2
+        Create skill module before instanitiate
+        psuedo code:
+            mob_skills = skill.Module(
+                SkillID: Skillfunc
+                ...
+            )
+            Mob(
+                ...,
+                skills=mob_skills
+            )
+
+    3. Independent motion control #1
+        psuedo code:
+            character.Motion(
+                group=...
+            )
+
+    4. Sound effect #0
+        Choose sound effect for actions.
+
+    5. Timeline and event trigger #1
+        event = timeline.Event(
+            time,
+            operation # Use functools.partial(...) to preform function.
+        )
+        event = timeline.Conditional(
+            cond, # or operation.
+            operation
+        )
+        trigger = timeline.Timeline.(events)
+        trigger.add(events)
+        trigger.start()
+
+    6. background #0
+        Choose images for background.
+
+    7. Add 'spawn_at()' to 'MobHandle' #0
+        ...
+'''
