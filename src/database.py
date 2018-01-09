7b9e51c416baea7b112b16b2cf262b2224f578cb
霧卦 <1103108147@gm.kuas.edu.tw>
2018-01-09 20:00:01 +0800

    完成資料庫的類別
    除了資料庫之後會根據需要儲存的資料做修改之外，資料庫類別大致完成

---
+++ src/database.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+class DataBase():
+
+    def __init__(self, name="score.db", column=""):
+        self.conn = sqlite3.connect( name )
+        self.cursor = self.conn.cursor()
+    
+    def SelectData(self):
+        order = "CREATE TABLE IF NOT EXISTS Score( name TEXT, time FLOAT)"
+        self.cursor.execute( order )
+        self.scores = self.cursor.execute("SELECT * FROM player order by time")
+        self.conn.commit()
+
+        return
+
+    def PrintData(self):
+        for item in self.scores:
+            print( item )
+        
+        return
+
+    def InsertData(self, name, time):
+        sql = "insert into player (name, time) values ('%s', '%s')" % (name, time)
+        self.cursor.execute( sql )
+        self.conn.commit()
+        return
+
+    def Close(self):
+        self.conn.close()
+        return
